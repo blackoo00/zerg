8334aba692a20020471c88367f22004141a6876f
blackoo00 <460544293@qq.com>
2019-07-01 16:10:26 +0800

    img

--- application/api/model/BaseModel.php
+++ application/api/model/BaseModel.php
@@ -9,9 +9,9 @@ class BaseModel extends Model
     //读取器（任何表的URL都会前缀处理）
     protected function prefixImgUrl($value,$data){
         $finalUrl = $value;
-        if($data['from'] == 1){
-            $finalUrl = config('setting.img_prefix').$value;
-        }
+        // if($data['from'] == 1){
+        //     $finalUrl = config('setting.img_prefix').$value;
+        // }
         if($data['from'] == config('setting.tencent_cloud_from')){
             $finalUrl = config('setting.tencent_cloud_prefix').$value;
         }
--- application/extra/setting.php
+++ application/extra/setting.php
@@ -7,7 +7,7 @@
  */
 return [
     'img_prefix' => 'http://a.cn/images',
-    'tencent_cloud_prefix' => 'http://zerg-1251466962.cossh.myqcloud.com',
-    'tencent_cloud_from' => 2,
+    'tencent_cloud_prefix' => 'https://zerg-1251466962.cos.ap-shanghai.myqcloud.com/public',
+    'tencent_cloud_from' => 1,
     'token_expire_in' => 7200
 ];
